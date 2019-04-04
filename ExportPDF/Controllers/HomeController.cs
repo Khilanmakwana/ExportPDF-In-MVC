0655d81f5a8a1f83a6b138b357f31e8e1d649d5f
Makwana Khilan <31216771+Khilanmakwana@users.noreply.github.com>
2019-04-04 08:34:05 +0530

    Add files via upload

---
+++ ExportPDF/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ExportPDF.Models;
+using Rotativa;
+namespace ExportPDF.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ProductModel pm = new ProductModel();
+            ViewBag.listProducts = pm.findAll();
+            return View();
+        }
+        public ActionResult ExportPDF()
+        {
+            return new ActionAsPdf("Index")
+            {
+                FileName = Server.MapPath("~/Content/ListProduct.pdf")
+            };
+        }
+    }
+}
---
+++ ExportPDF/Models/ProductModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExportPDF.Models
+{
+    public class ProductModel
+    {
+        public List<Product> findAll()
+        {
+            List<Product> result = new List<Product>();
+            result.Add(new Product { Id = "p01", Name = "Product 1", Price = 100, Photo = "1.PNG" });
+            result.Add(new Product { Id = "p02", Name = "Product 2", Price = 200, Photo = "2.JPG" });
+            return result;
+        }
+    }
+}
